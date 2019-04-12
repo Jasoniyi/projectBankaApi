5ac194083e240340e7c87d4774db9b5a9ee3efb9
CITADEL OF ROYALTY <josephomoniyitope@gmail.com>
2019-04-12 13:41:37 +0100

    feature(signup): create signin page[continue #165256898]

---
+++ __test__/users.test.js
@@ -0,0 +1,164 @@
+/* eslint-disable no-undef */
+import chaiHttp from "chai-http";
+import chai, { expect } from "chai";
+
+import app from "../app";
+
+chai.use(chaiHttp);
+
+describe("Testing user controller", () => {
+  describe("Testing signup controller", () => {
+    const signupUrl = "/api/v1/signup";
+    it("should register a user when all the parameters are given", done => {
+      chai
+        .request(app)
+        .post(signupUrl)
+        .send({
+          id: 1,
+          firstName: "bill",
+          lastName: "gates",
+          email: "bill@gates.com",
+          password: "password",
+          confirmPassword: "password"
+        })
+        .end((error, res) => {
+          console.log(res.body);
+          expect(res.body).to.be.an("object");
+          expect(res).to.have.status(201);
+          expect(res.body.status).to.equal(201);
+          expect(res.body.data).to.be.an("object");
+          expect(res.body.data).to.have.property("token");
+          expect(res.body.data).to.have.property("id");
+          expect(res.body.data).to.have.property("firstName");
+          expect(res.body.data).to.have.property("lastName");
+          expect(res.body.data).to.have.property("email");
+          expect(res.body.data).to.have.property("password");
+          expect(res.body.data.token).to.be.a("string");
+          expect(res.body.data.email).to.be.equal("bill@gates.com");
+          done();
+        });
+    });
+
+    it("should not register a user when the email is missing", (done) => {
+      chai
+        .request(app)
+        .post(signupUrl)
+        .send({
+          firstName: "bill",
+          lastName: "gates",
+          password: "password",
+          confirmPassword: "password"
+        })
+        .end((error, res) => {
+          expect(res.body).to.be.an("object");
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.be.a("string");
+          expect(res.body.error).to.equal("email is required");
+          done();
+        });
+    });
+
+      it('should not register a user when the firstname is missing', 
+        (done) => {
+            chai.request(app)
+            .post(signupUrl)
+            .send({
+              lastName: 'gates',
+              email: 'bill@gates.com',
+              password: 'password',
+              confirmPassword: 'password',
+            })
+            .end((error, responce) => {
+              expect(responce.body).to.be.an('object');
+              expect(responce).to.have.status(400);
+              expect(responce.body.error).to.be.a('string');
+              expect(responce.body.error).to.equal('Firstname is required');
+              done();
+            })
+      },
+    );
+
+    it('should not register a user when the lastname is missing',
+        (done) => {
+            chai.request(app)
+            .post(signupUrl)
+            .send({
+              firstName: 'bill',
+              email: 'bill@gates.com',
+              password: 'password',
+              confirmPassword: 'password',
+            })
+            .end((error, responce) => {
+              expect(responce.body).to.be.an('object');
+              expect(responce).to.have.status(400);
+              expect(responce.body.error).to.be.a('string');
+              expect(responce.body.error).to.equal('Lastname is required');
+              done();
+            })
+      },
+    );
+
+    it('should not register a user when the password is missing',
+        (done) => {
+            chai.request(app)
+            .post(signupUrl)
+            .send({
+              firstName: 'bill',
+              lastName: 'gates',
+              email: 'bill@gates.com',
+              confirmPassword: 'password',
+            })
+            .end((error, responce) => {
+              expect(responce.body).to.be.an('object');
+              expect(responce).to.have.status(400);
+              expect(responce.body.error).to.be.a('string');
+              expect(responce.body.error).to.equal('password is required');
+              done();
+            })
+      },
+    );
+
+    it('should not register a user when the passwords do not match',
+        (done) => {
+            chai.request(app)
+            .post(signupUrl)
+            .send({
+              firstName: 'bill',
+              lastName: 'gates',
+              email: 'bill@gates.com',
+              password: 'password',
+              confirmPassword: 'passwords do not match',
+            })
+            .end((error, responce) => {
+              expect(responce.body).to.be.an('object');
+              expect(responce).to.have.status(400);
+              expect(responce.body.error).to.be.a('string');
+              expect(responce.body.error).to.equal('passwords do not match');
+              done();
+            })
+      },
+    );
+
+    // it('should not register a user with an already existing email', (done) => {
+    //     (done) => {
+    //         chai.request(app)
+    //         .post(signupUrl)
+    //         .send({
+    //           firstName: 'bill',
+    //           lastName: 'gates',
+    //           email: 'bill@gates.com',
+    //           password: 'password',
+    //           confirmPassword: 'password',
+    //         })
+    //         .end((error, responce) => {
+    //           expect(responce.body).to.be.an('object');
+    //           expect(responce).to.have.status(400);
+    //           expect(responce.body.error).to.be.a('string');
+    //           expect(responce.body.error).to.equal('Email already exists');
+    //           done();
+    //         })
+    //     }
+    //   },
+    // );
+  });
+});
---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+import moment from "moment";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "secret";
+
+class UserController {
+  static signup(req, res) {
+    const {
+      firstName, lastName, email, password, confirmPassword 
+    } = req.body
+    if (!firstName) {
+      return res.status(400).json({
+        status: 400,
+        error: "Firstname is required"
+      });
+    }
+    if (!lastName) {
+      return res.status(400).json({
+        status: 400,
+        error: "Lastname is required"
+      });
+    }
+    if (!email) {
+      return res.status(400).json({
+        status: 400,
+        error: "email is required"
+      });
+    }
+    if (!password) {
+      return res.status(400).json({
+        status: 400,
+        error: "password is required"
+      });
+    }
+    if (password != confirmPassword) {
+      return res.status(400).json({
+        status: 400,
+        error: "passwords do not match"
+      });
+    
+    }
+
+    const token = jwt.sign({ data: req.body.firstName }, secret, {
+      expiresIn: "1h"
+    });
+
+    const userSchema = {
+      id: req.body.id,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, salt),
+      type: "user",
+      dateRegistered: moment().format(),
+      isAdmin: false,
+      token
+    };
+    return res.status(201).json({ status: 201, data: { ...userSchema } });
+  }
+}
+
+export default UserController;
